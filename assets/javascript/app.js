716881ff8281589de45cdb69ecd855fc380ec421
Rodolfo <jrsotorodolfo@gmail.comgit config --global user.email jrsotorodolfo@gmail.comgit config --global user.email jrsotorodolfo@gmail.com>
2019-01-17 18:35:25 -0600

    Most of the logic completed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,9 @@
-//Create an arry of strings related to my topic of NBA players
-// var nbaPlayers = ["Michael Jordan", "Allen Iverson", "Dennis Rodman", "Vince Carter"];
+//Create an arry of strings related to my topic of NBA playerss
+// var nbaplayerss = ["Michael Jordan", "Allen Iverson", "Dennis Rodman", "Vince Carter"];
 
 //Create a button for each string and append the button for each string
 // var createBtn = function(input){
-//     for(var i = 0; i < nbaPlayers.length; i++)
+//     for(var i = 0; i < nbaplayerss.length; i++)
     
 //     $("#addSearch").text(createBtn);
 
@@ -14,40 +14,81 @@
 
 //On click function that retreives 10 gifs
 
-var player = "Michael Jordan";
-var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + player + "&api_key=bWRree7iP0V0neZViCSJ3wUU4DANrTY4&limit=10"
+var players = ["Michael Jordan", "Dennis Rodman"];
+var player;
+
+
+
+function displayGif(){
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + player + "&api_key=bWRree7iP0V0neZViCSJ3wUU4DANrTY4&limit=10";
+
+    $("#searches").empty();
+
+
 $.ajax({
     url: queryURL,
     method: "GET"
 }).then(function(response) {
+    for(var i = 0; i < response.data.length; i++){
     // console.log(response);
-    // console.log(response.data[3].images.fixed_height.url);
-    // console.log(response.data[3].rating)
+    // console.log(response.data[i].images.fixed_height.url);
+    console.log(response.data)
+    }
     //this for loop is to loop through the entire dot notation of the JSON documnet
-    for(var i = 0; i < response.data[i].length; i++){
+    for(var i = 0; i < response.data.length; i++){
         //create variables and attributes to pull desired data from the JSON
         //in this case I am looking for the rating, the gif URL, and an image for my gif
         var rating = response.data[i].rating;
-        var p = $("<p>").text('Rating' + rating);
+        var p = $("<p>").text("Rating " + rating);
         var animated = response.data[i].images.fixed_height.url;
+        var still = response.data[i].images.fixed_height_still.url;
         var image = $("<img>");
-        image.attr("data-animated", animated)
+        image.attr("src", still);
+        image.attr("data-animated", animated);
+        image.attr("data-stil", still)
         $("#searches").append(image);
         $("#searches").append(p);
         console.log(rating);
         console.log(animated);
-    }
-    
+    };
 
-    
+    console.log(response);
+
 });
+
+
+}
 
 //Create function that animates the gif once it is clicked
 //If the user clicks it again, it stops the animation
 
+function renderBtn (){
+    for(var i=0; i < players.length; i++){
+        var btn = $("<button>");
+        btn.text(players[i]); 
+        btn.addClass("gif-btn");
+        $("#btnArea").append(btn);
+
+        console.log(btn);   
+   
+}
+};
+renderBtn();
+
+//create a funciton that passes an on click event, grabs the text of the button, and displays the Gif'
+
+$(document).on("click", ".gif-btn", function(){
+    player = $(this).text();
+    displayGif();
+
+});
+
+
+
+
 //From the JSON, pull and display the rating
 //Create a function that uses .val().trim() methods to pull the user inpur data and add it 
-//to the nbaPlayers variable
+//to the nbaplayerss variable
 
 $("#searches").append("<input type='text'>");
 
